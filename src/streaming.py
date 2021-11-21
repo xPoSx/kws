186bc05b155cfd65ae103cb0fcef13b92dfd85ae
victorgri <vsgrishanin@edu.hse.ru>
2021-11-22 02:13:53 +0300

    fix

--- src/streaming.py
+++ src/streaming.py
@@ -1,23 +1,34 @@
 import torch
-import torch.nn.functional as F
+from src.baseline_model import CRNN
 
 
-class StreamingCRNN:
-    def __init__(self, max_window_length, step_size):
+class StreamingCRNN(CRNN):
+    def __init__(self, max_window_length, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.max_window_length = max_window_length
-        self.step_size = step_size
 
-    @torch.no_grad()
-    def validation(self, model, input):
-        model.eval()
-        hidden_state = None
-        probs = []
+    def forward(self, input):
+        if self.training:
+            return super(StreamingCRNN, self).forward(input)
+        else:
+            gru_buf = []
+            hidden_state = None
+            outputs = []
+            i = 0
+            while i < input.shape[1] - self.config.kernel_size[1]:
 
-        for i in range(0, input.shape[1], self.step_size):
-            if i + 20 > input.shape[1]:
-                break
+                conv_input = input[:, i:i + self.config.kernel_size[1]].unsqueeze(0)
 
-            output, hidden_state = model(input[:, i:i + self.max_window_length].unsqueeze(0), hidden_state, True)
-            probs.append(F.softmax(output, dim=-1)[0][1])
+                conv_input = conv_input.unsqueeze(dim=1)
+                conv_output = self.conv(conv_input).transpose(-1, -2)
+                gru_output, hidden_state = self.gru(conv_output, hidden_state)
+                gru_buf.append(gru_output)
 
-        return probs
+                if i + self.config.kernel_size[1] >= self.max_window_length:
+                    attn_inp = torch.cat(gru_buf, dim=1)
+                    contex_vector = self.attention(attn_inp)
+                    outputs.append(self.classifier(contex_vector))
+                    gru_buf.pop(0)
+                i += self.config.stride[1]
+
+            return outputs
